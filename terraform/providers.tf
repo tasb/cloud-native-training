a100fc92fa36317d1e0a9df64138c5f27f1e8d35
Tiago Bernardo <tiago_bernardo@hugoboss.com>
2026-03-06 01:15:07 +0000

    fix credscan

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.90"
+      version = "~> 3.95"
     }
     random = {
       source  = "hashicorp/random"
